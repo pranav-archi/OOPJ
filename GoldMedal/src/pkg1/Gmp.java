07efe3551b0ad9317d11dbbfc501881b1caff9fd
pranav-archi <158431652+pranav-archi@users.noreply.github.com>
2024-06-12 20:04:47 +0530

    Update Gmp.java
    Minor changes in code

--- GoldMedal/src/pkg1/Gmp.java
+++ GoldMedal/src/pkg1/Gmp.java
@@ -14,8 +14,19 @@ public class Gmp {
 			li1 = line.split(",");
 			int engMark = Integer.parseInt(li1[3].split(":")[1]);
 			englishMarks.add(engMark);
-			
+			int mathMark = Integer.parseInt(li1[4].split(":")[1]);
+			mathsMarks.add(mathMark);
+			int phyMark = Integer.parseInt(li1[5].split(":")[1]);
+			physicsMarks.add(phyMark);
+			int chemMark = Integer.parseInt(li1[6].split(":")[1]);
+			chemistryMarks.add(chemMark);
+			int bioMark = Integer.parseInt(li1[7].split(":")[1]);
+			biologyMarks.add(bioMark);
 		}
 		System.out.println(englishMarks);
+		System.out.println(mathsMarks);
+		System.out.println(physicsMarks);
+		System.out.println(chemistryMarks);
+		System.out.println(biologyMarks);
 	}
 }
